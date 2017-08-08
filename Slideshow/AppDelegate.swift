80b353efc029ae25191468f55d18fe88fef41f31
Dave Rogers <dave@drogers.us>
2017-08-08 02:52:28 -0700

    Initial Commit

---
+++ Slideshow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Slideshow
+//
+//  Created by David Rogers on 8/8/17.
+//  Copyright © 2017 David Rogers. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
