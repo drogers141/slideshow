37a12c77dcd459635edf977d4a9a90dd618e0e7f
Dave Rogers <dave@drogers.us>
2017-08-09 02:51:31 -0700

    add splitview and subclass

---
+++ Slideshow/TopViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TopViewController.swift
+//  Slideshow
+//
+//  Created by David Rogers on 8/9/17.
+//  Copyright © 2017 David Rogers. All rights reserved.
+//
+
+import Cocoa
+
+class TopViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+}
+
